2988107952cf1f2f2e0779cf74e824758716b6fa
Ivan Safronov <idsafronov@dev.vtb.ru>
2025-06-19 20:27:57 +0300

    Добавил Entity Employee.java

---
+++ src/main/java/com/safronov/spring/mvc/entity/Employee.java
@@ -0,0 +1,30 @@
+package com.safronov.spring.mvc.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "employees")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @Column(name = "name", length = 15)
+    private String name;
+
+    @Column(name = "surname", length = 25)
+    private String surname;
+
+    @Column(name = "department", length = 20)
+    private String department;
+
+    @Column(name = "salary")
+    private Integer salary;
+
+}
