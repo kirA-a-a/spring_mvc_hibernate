8ca0d2bd62cd80f3cefd2dd06cfd4ac0554e78e7
Ivan Safronov <idsafronov@dev.vtb.ru>
2025-06-20 16:05:12 +0300

    Изменение конфигурации проекта, запуск на веб

--- src/main/java/com/safronov/spring/mvc/controller/MyController.java
+++ src/main/java/com/safronov/spring/mvc/controller/MyController.java
@@ -3,13 +3,14 @@ package com.safronov.spring.mvc.controller;
 import com.safronov.spring.mvc.dao.EmployeeDAO;
 import com.safronov.spring.mvc.entity.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
-@org.springframework.stereotype.Controller
-public class Controller {
+@Controller
+public class MyController {
 
     @Autowired
     private EmployeeDAO employeeDAO;
