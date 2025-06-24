fbbe7668b6380a494076661e2da04e123df758f9
Ivan Safronov <idsafronov@dev.vtb.ru>
2025-06-24 20:38:21 +0300

    Рефакторинг EmployeeDAOImpl

--- src/main/java/com/safronov/spring/mvc/dao/EmployeeDAOImpl.java
+++ src/main/java/com/safronov/spring/mvc/dao/EmployeeDAOImpl.java
@@ -5,11 +5,10 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
-@Repository
+@Repository()
 public class EmployeeDAOImpl implements EmployeeDAO {
 
     @Autowired
@@ -17,7 +16,7 @@ public class EmployeeDAOImpl implements EmployeeDAO {
 
     @Override
     public List<Employee> getAllEmployees() {
-         Session session = sessionFactory.getCurrentSession();
-         return session.createQuery("from Employee", Employee.class).getResultList();
+        Session session = sessionFactory.getCurrentSession();
+        return session.createQuery("from Employee", Employee.class).getResultList();
     }
 }
